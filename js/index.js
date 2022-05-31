a7ae56577fe6157fdcd6847a0d47acb67f38592b
manu <manufritzenpro@gmail.com>
2022-05-31 19:23:30 -0300

    js adjustments

--- js/index.js
+++ js/index.js
@@ -1,30 +1,12 @@
-const routes = {
-  '/': '/pages/home.html',
-  '/theUniverse': '/pages/theUniverse.html',
-  '/exploration': '/pages/exploration.html',
-  404: '/pages/404.htm'
-}
-function route(event) {
-  event = event || window.event
-  event.preventDefault()
+import { Router } from'./rauter.js'
 
-  window.history.pushState({}, "", event.target.href)
+const router = new Router()
+router.add('/', "/pages/home.html")
+router.add('/theUniverse', "/pages/theUniverse.html")
+router.add('/exploration', "/pages/exploration.html")
+router.add(404, "/pages/404.htm")
 
-  handle()
-}
+router.handle()
 
-function handle() {
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route)
-  .then(data => data.text())
-  .then(html => {
-    document.querySelector('#app').innerHTML = html
-  })
-}
-
-handle()
-
-window.onpopstate = () => handle()
-window.route = () => route()
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
